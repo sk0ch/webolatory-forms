6d8225edf3cfc7c9d898e0d6ae5127108559c92a
sk0ch <skoch.ue@gmail.com>
2018-06-06 17:03:23 +0300

    Main methods

--- WebolatoryForms.php
+++ WebolatoryForms.php
@@ -12,25 +12,69 @@
 class WebolatoryForms {
 
     /**
-     * Form
+     * Form id
+     *
+     * @var string
+     */
+	var $form_id = null;
+
+    /**
+     * Form fields
      *
      * @var array
      */
-	public $form = array();
+	var $form_fields = array();
+
+	/**
+	* Set form id
+	*
+	* @return void.	
+	*/
+	public function setFormId( $id ) {
+
+		$this->form_id = $id;
+	}
 
 	/**
 	* Add field to form.
 	*
+	* @param string $id Field id.
 	* @param string $type Field type.
 	* @param string $name Field name.
-	* @param string $id Field id.
 	* @param string $class Field class.
 	* @param string $value Field value.
 	* @param boolenan $required Required field.
 	*
-	* return void.	
+	* @return void.	
 	*/
-	function AddField( $type, $name, $id = null, $class = null, $value = null, $required = false ) {
+	public function addField( $id, $type, $name, $class = null, $value = null, $required = false ) {
 
+		$this->form_fields[ $id ] = array(
+			'type'		=> $type,
+			'name'		=> $name,
+			'id'		=> $id,
+			'class'		=> $class,
+			'value'		=> $value,
+			'required'	=> $required,
+		);
+	}
+
+	/**
+	* Show form.
+	*
+	* @return string Display in HTML.	
+	*/
+	public function getForm() {
+		
+	}
+
+	/**
+	* Show form.
+	*
+	* @return void.	
+	*/
+	public function showForm() {
+
+		echo $this->getForm();
 	}
 }
