f2c794e5ed71e04a3a18abf5d9b39c2f5e1b012c
sk0ch <skoch.ue@gmail.com>
2018-06-06 16:37:18 +0300

    Init

---
+++ WebolatoryForms.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Webolatory Forms.
+ *
+ * @package		Webolatory Forms
+ * @author		Andrew Skochelias
+ */
+
+/**
+ * Class WebolatoryForms.
+ */
+class WebolatoryForms {
+
+    /**
+     * Form
+     *
+     * @var array
+     */
+	public $form = array();
+
+	/**
+	* Add field to form.
+	*
+	* @param string $type Field type.
+	* @param string $name Field name.
+	* @param string $id Field id.
+	* @param string $class Field class.
+	* @param string $value Field value.
+	* @param boolenan $required Required field.
+	*
+	* return void.	
+	*/
+	function AddField( $type, $name, $id = null, $class = null, $value = null, $required = false ) {
+
+	}
+}
